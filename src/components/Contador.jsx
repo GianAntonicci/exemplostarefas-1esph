c13549969ced300a5f9a5e071f1856a031a57f26
gianluca.antonicci@gmail.com <gianluca.antonicci@gmail.com>
2026-09-14 23:01:24 -0300

    style: Estilização do projeto com tailwindcss

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,17 +1,14 @@
-// useState = Hooks
 import {useState} from 'react'
-
 
 const Contador = () => {
 
-// Hook - useState - manipula o estado da variável
-const [contador, setContador]=useState(0);
-
+// HOOK- useState - Manipula o estado da variável
+const [contador,setContador]=useState(0);
 
   return (
     <>
-        <h1>Contagem inicial: {contador} </h1>
-        <button onClick={()=>setContador(contador +1)}>Aumentar</button>
+      <h1>Contagem Inicial:{contador}</h1>
+    <button onClick={()=>setContador(contador +1)}>Aumentar</button>
     </>
   )
 }
--- src/components/Tarefas.jsx
+++ src/components/Tarefas.jsx
@@ -1,80 +1,86 @@
-// useEffect - Atualiza na hora o que foi cadastrado, não precisa dar F5
 import {useState, useEffect} from 'react'
-import Contador from '../components/Contador.jsx'
 import '../css/style.css'
 
-const Tarefas = () => {
+const Tarefa = () => {
 
-        // Hook - useState - manipula o estado da variável
-        const [tarefas, setTarefas]=useState(()=>{
-            const salvarTarefas = localStorage.getItem("item-tarefa");
-            return salvarTarefas ? JSON.parse(salvarTarefas) : [];
-        });
+    //Hook- useState -Manipula o estado da variável
+    const [tarefas,setTarefas]=useState(()=>{
+        const salvarTarefas = localStorage.getItem("item-tarefa");
+        return salvarTarefas ? JSON.parse(salvarTarefas) : [];
+    });
 
-        const [campo, setCampo]=useState("");
-        // HOOk - useEffect - Realiza o efeito colateral, nesse exemplo vai mostrar a tarefa adicionada em tempo real
-        useEffect(()=>{
-            localStorage.setItem("item-tarefa", JSON.stringify(tarefas))
-        },[tarefas])
+    const [campo,setCampo]=useState("");
 
-        // Função Adicionar Tarefa
-        const AdicionarTarefa = (e)=> {
-          //preventDefault - Previne load sozinho da página
-          e.preventDefault();
-          // trim() = remove espaços em branco (igual strip() do python)
-          // Validação de campo se o campo estiver vazio
-          if(!campo.trim()) return;
+    //HOOK - useEffect - Realiza o efeito colateral, nesse exmeplo
+    //vai mostrar a tarefa adicionada em tempo real
+    useEffect(()=>{
+        localStorage.setItem("item-tarefa",JSON.stringify(tarefas))
+    },[tarefas])
 
-          // novo objeto - tudo aquilo que se pode dar características ou propriedades
-          const novaTarefa = {
-            // Date.now = Define um id automaticamente
-            id:  Date.now(),
-            texto: campo,
-          }
-          // ... = Spred - Adiciona tarefas e mantem as antigas
-          setTarefas([...tarefas,novaTarefa]);
-          // Limpa a tela
-          setCampo('');
-        }
+    //FUNÇÃO ADICIONAR TAREFA
+    const  AdicionarTarefa =(e)=>{
+      //Previne que a página se recarregue automaticamente
+      e.preventDefault();
+      //Valida se o campo estiver vazio
+      if(!campo.trim()) return;
 
-        // Função remover tarefa
-        const RemoverTareda = (id) => {
-          // Verifica se o ID da tarefa atual é diferente do ID que deja apagar
-          // Se o ID for igual (tarefa desesja apagar) a condição retorna falso
-          // E o item é excluido
-          const apagarTarefa = tarefas.filter((tarefa) => tarefa.id != id);
-          setTarefas(apagarTarefa);
-        }
+      //novo objeto
+      const novaTarefa={
+        id: Date.now(),
+        texto:campo,
+      }
+      setTarefas([...tarefas,novaTarefa]);
+      setCampo('');
+    }
+    //FUNÇÃO REMOVER TAREFA
+    const RemoverTarefa=(id)=>{
+      //VERIFICA SE O ID DA TAREFA ATUAL É DIFERENTE DO ID QUE DESEJA APAGAR
+      //SE O ID FOR IGUAL(TAREFA QUE DESEJA APAGAR) A CONDIÇÃO RETORNA FALSO 
+      //E O ITEM É EXCLUIDO
+      const apagarTarefa = tarefas.filter((tarefa)=> tarefa.id !== id);
+      setTarefas(apagarTarefa);
+
+    }
+
 
   return (
-    <>
-      <div className='todo-container'>
-        <h1>Minha lista de tarefas</h1>
-        <form onSubmit={AdicionarTarefa}>
-          <input
-            type='text'
-            value={campo}
-            onChange={(e)=> setCampo(e.target.value)}
-            placeholder='Digite sua Tarefa'
-            className='todo-input'
-          />
-          <button type='submit'>Adicionar</button>
-        </form>
-      </div>
+    <div className="max-w-md mx-auto mt-10 bg-indigo-500 rounded-2xl  shadow-lg shadow-amber-200 border border-white">
+      <h1 className="text-2xl font-bold text-white mb-6 text-center">Minha Lista de Tarefas</h1>
+      <form onSubmit={AdicionarTarefa} className="flex gap-2 mb-6">
+        <input
+          type="text"
+          value={campo}
+          onChange={(e)=>setCampo(e.target.value)}
+          placeholder='Digite sua Tarefa'
+          className="flex-1 px-4 py-2 border border-gray-700 rounded-2xl focus:outline-none focus:ring-1
+           focus:ring-amber-200 focus:border-transparent text-black placeholder:text-gray-700"
+        />
+        <button type="submit"
+          className="bg-indigo-950 hover:bg-indigo-400 text-amber-300  
+          font-medium px-5 py-2 rounded-2xl transition-colors cursor-pointer"
+        >Adicionar</button>
+      </form>
 
-      <ul>
+      <ul className="space-y-3">
         {tarefas.map((tarefa)=>(
-          <li key={tarefa.id}>
+          <li key={tarefa.id} 
+          className="flex items-center justify-between p-3 bg-indigo-900 border
+           border-amber-300 rounded-2xl shadow-sm
+            hover:bg-indigo-500 transition-colors" >
             <span>{tarefa.texto}</span>
-            <button onClick={() => RemoverTareda(tarefa.id)}>Excluir</button>
+            <button onClick={()=>RemoverTarefa(tarefa.id)}
+            className="bg-red-950 hover:bg-red-400 text-amber-50  
+            font-medium px-5 py-2 rounded-2xl transition-colors cursor-pointer"
+              >Excluir</button>
           </li>
         ))}
       </ul>
-      {/* Compara se nao tiver tarefas deixa a mensagem nenhuma tarefa salva */}
-      {tarefas.length === 0 && <p>Nenhuma Tarefa Salva</p>}
-      <Contador/>
-    </>
+      {/* COMPARA SE NÃO TIVER TAREFAS DEIXA A MENSAGEM NENHUMA TAREFA SALVA */}
+      {tarefas.length === 0 && <p className=" text-center italic mt-4">Nenhuma Tarefa Salva</p>}
+    
+      
+    </div>
   )
 }
 
-export default Tarefas
+export default Tarefa
